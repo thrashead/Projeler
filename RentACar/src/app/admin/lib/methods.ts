cba6083fe15677b50a6db528c69c6d7144c1788f
SINA SALIK <sinasalik@hotmail.com>
2019-10-09 23:08:13 +0300

    Admin lib'de CKValues null hatası düzeltildi.

--- RentACar/src/app/admin/lib/methods.ts
+++ RentACar/src/app/admin/lib/methods.ts
@@ -3,41 +3,44 @@ import ClassicEditor from "../../../../Content/admin/js/ckeditor/ckeditor.js";
 
 @Injectable({ providedIn: 'root' })
 export class AdminLib {
-	static UploadFileName(filename: string, guidCount: number = 5) {
-		let x: string = "";
-		let ext: string = filename.split('.').pop();
-		let name: string = filename.replace("." + ext, "");
-		let guid: string;
+    static UploadFileName(filename: string, guidCount: number = 5) {
+        let x: string = "";
+        let ext: string = filename.split('.').pop();
+        let name: string = filename.replace("." + ext, "");
+        let guid: string;
 
-		for (var i = 0; i < guidCount; i++) {
-			x += "x";
-		}
+        for (var i = 0; i < guidCount; i++) {
+            x += "x";
+        }
 
-		guid = x.replace(/[x]/g, function (c) {
-			var r = Math.random() * 16 | 0,
-				v = c == 'x' ? r : (r & 0x3 | 0x8);
-			return v.toString(16);
-		});
+        guid = x.replace(/[x]/g, function (c) {
+            var r = Math.random() * 16 | 0,
+                v = c == 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
 
-		return name + "-" + guid + "." + ext;
-	}
-
-	static CKValue(id: string) {
-		return $(".ck-content[data-id='" + id + "']").html().replace("<p>", "").replace("</p>", "");
-	}
-
-	static ConvertToCKEditor(id: string, time: number = 1000) {
-		setTimeout(function () {
-			ClassicEditor
-				.create(document.querySelector("#" + id), {
-				})
-				.then(editor => {
-					console.log(editor);
-				});
-		}, time);
+        return name + "-" + guid + "." + ext;
     }
 
-    static ParseFloat(value: string) : any {
+    static CKValue(id: string) {
+        if ($(".ck-content[data-id='" + id + "']").html() == null)
+            return "";
+        else
+            return $(".ck-content[data-id='" + id + "']").html().replace("<p>", "").replace("</p>", "");
+    }
+
+    static ConvertToCKEditor(id: string, time: number = 1000) {
+        setTimeout(function () {
+            ClassicEditor
+                .create(document.querySelector("#" + id), {
+                })
+                .then(editor => {
+                    console.log(editor);
+                });
+        }, time);
+    }
+
+    static ParseFloat(value: string): any {
         var returnValue;
 
         if (value == null || value == undefined) {
